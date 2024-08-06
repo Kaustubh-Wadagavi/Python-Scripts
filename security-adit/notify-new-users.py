08fd79df67b9a3200d4a44c845dcda3da3255dc5
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2024-08-06 15:42:37 +0530

    Error notification function implemented.

--- security-adit/notify-new-users.py
+++ security-adit/notify-new-users.py
@@ -1,32 +1,19 @@
 import requests
 import argparse
 import json
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import ssl 
+import ssl
 
-def sendEmail(firstName, lastName, url, senderEmail, receiverEmail, emailPassword):
+def sendEmail(subject, body, senderEmail, receiverEmail, emailPassword):
     message = MIMEMultipart("alternative")
-    message["Subject"] = "New User Created Today"
+    message["Subject"] = subject
     message["From"] = senderEmail
     message["To"] = receiverEmail
 
-    html = f"""
-    <html>
-        <body>
-            <p>Hello,<br><br>
-               The new user is created.<br><br>
-               First Name: {firstName}<br>
-               Last Name: {lastName}<br><br>
-               Visit <a href="{url}">this link</a> for more details.<br><br>
-               Thanks.
-           </p>
-       </body>
-       </html>
-    """
-    part = MIMEText(html, "html")
+    part = MIMEText(body, "html")
     message.attach(part)
 
     context = ssl._create_unverified_context()
@@ -40,49 +27,81 @@ def sendEmail(firstName, lastName, url, senderEmail, receiverEmail, emailPasswor
     except Exception as e:
         print(f"Failed to send email: {e}")
 
+def sendErrorEmail(error_message, senderEmail, receiverEmail, emailPassword):
+    subject = "Script Error Notification"
+    body = f"""
+    <html>
+        <body>
+            <p>Hello,<br><br>
+               An error occurred in the script.<br><br>
+               Error Message: {error_message}<br><br>
+               Thanks.
+           </p>
+       </body>
+       </html>
+    """
+    sendEmail(subject, body, senderEmail, receiverEmail, emailPassword)
+
+def sendNewUserEmail(firstName, lastName, url, senderEmail, receiverEmail, emailPassword):
+    subject = "New User Created Today"
+    body = f"""
+    <html>
+        <body>
+            <p>Hello,<br><br>
+               A new user was created.<br><br>
+               First Name: {firstName}<br>
+               Last Name: {lastName}<br><br>
+               Visit <a href="{url}">this link</a> for more details.<br><br>
+               Thanks.
+           </p>
+       </body>
+       </html>
+    """
+    sendEmail(subject, body, senderEmail, receiverEmail, emailPassword)
+
 def checkUserCreationDate(users, url, token, senderEmail, receiverEmail, emailPassword):
     yesterday = (datetime.now(timezone.utc) - timedelta(days=1)).date()
     for user in users:
         if 'creationDate' in user:
             createdDate = datetime.fromtimestamp(user['creationDate'] / 1000, timezone.utc).date()
             if createdDate == yesterday:
-                sendEmail(user['firstName'], user['lastName'], url, senderEmail, receiverEmail, emailPassword)
+                sendNewUserEmail(user['firstName'], user['lastName'], url, senderEmail, receiverEmail, emailPassword)
                 print(f"User created yesterday: {user['firstName']} {user['lastName']}")
         else:
             continue
 
-def getUsers(url, token):
+def getUsers(url, token, senderEmail, receiverEmail, emailPassword):
     headers = {'X-OS-API-TOKEN': token}
     start = 0
     max_results = 100
     while True:
-        response = requests.get(f"{url}rest/ng/users?start={start}&max={max_results}", headers=headers)
-        if response.status_code == 200:
+        try:
+            response = requests.get(f"{url}rest/ng/users?start={start}&max={max_results}", headers=headers)
+            response.raise_for_status()
             users = response.json()
             if not users:
                 break
-            checkUserCreationDate(users, url, token, config['senderEmail'], config['receiverEmail'], config['emailPassword'])
+            checkUserCreationDate(users, url, token, senderEmail, receiverEmail, emailPassword)
             start += 100
-            max_results += 100
-        else:
-            print(f"Failed to retrieve users: {response.status_code}")
-            print("Response:", response.text)
+        except requests.exceptions.RequestException as e:
+            error_message = f"Failed to retrieve users: {str(e)}"
+            sendErrorEmail(error_message, senderEmail, receiverEmail, emailPassword)
             break
 
-def getToken(loginName, password, url):
+def getToken(loginName, password, url, senderEmail, receiverEmail, emailPassword):
     payload = {
         "loginName": loginName,
         "password": password
     }
 
-    response = requests.post(url + 'rest/ng/sessions', json=payload)
-
-    if response.status_code == 200:
+    try:
+        response = requests.post(url + 'rest/ng/sessions', json=payload)
+        response.raise_for_status()
         data = response.json()
         return data['token']
-    else:
-        print(f"Failed to retrieve token: {response.status_code}")
-        print("Response:", response.text)
+    except requests.exceptions.RequestException as e:
+        error_message = f"Failed to retrieve token: {str(e)}"
+        sendErrorEmail(error_message, senderEmail, receiverEmail, emailPassword)
         return None
 
 def readConfig(configFile):
@@ -94,10 +113,14 @@ def readConfig(configFile):
     return config
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Notify new users and get token')
-    parser.add_argument('configFile', type=str, help='Path to the configuration file')
-    args = parser.parse_args()
-    config = readConfig(args.configFile)
-    token = getToken(config['loginName'], config['password'], config['url'])
-    if token:
-        getUsers(config['url'], token)
+    try:
+        parser = argparse.ArgumentParser(description='Notify new users and get token')
+        parser.add_argument('configFile', type=str, help='Path to the configuration file')
+        args = parser.parse_args()
+        config = readConfig(args.configFile)
+        token = getToken(config['loginName'], config['password'], config['url'], config['senderEmail'], config['receiverEmail'], config['emailPassword'])
+        if token:
+            getUsers(config['url'], token, config['senderEmail'], config['receiverEmail'], config['emailPassword'])
+    except Exception as e:
+        error_message = str(e)
+        sendErrorEmail(error_message, config['senderEmail'], config['receiverEmail'], config['emailPassword'])
