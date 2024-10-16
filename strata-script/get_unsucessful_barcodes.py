bf1e82690e00e3c31120d8d8a4b56964f89162d0
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2024-10-16 16:48:12 +0530

    1. Added receive time in the CSV. 2. Added timeframe, current date - 1 day. So it sends yesterdays failed barcodes list. 3. Added email functionality.

--- strata-script/get_unsucessful_barcodes.py
+++ strata-script/get_unsucessful_barcodes.py
@@ -1,21 +1,64 @@
+#!/usr/bin/env python3
+
+import os
 import argparse
 import csv
+import ssl
 import mysql.connector as mc
 from mysql.connector import Error
-from datetime import datetime
+from datetime import datetime, timedelta
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+def sendEmail(subject, body, senderEmail, receiverEmail, emailPassword, attachment):
+    message = MIMEMultipart("mixed")
+    message["Subject"] = subject
+    message["From"] = senderEmail
+    message["To"] = receiverEmail
+
+    part = MIMEText(body, "html")
+    message.attach(part)
+
+    try:
+        with open(attachment, "rb") as file:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(file.read())
+            encoders.encode_base64(part)
+            part.add_header(
+                "Content-Disposition",
+                f"attachment; filename={os.path.basename(attachment)}",
+            )
+            message.attach(part)
+    except Exception as e:
+        print(f"Failed to attach file: {e}")
+
+    context = ssl._create_unverified_context()
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls(context=context)
+            server.login(senderEmail, emailPassword)
+            server.sendmail(senderEmail, receiverEmail, message.as_string())
+            print("Email sent successfully!")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
 
 def write_to_csv(data, output_file):
     with open(output_file, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['No specimens with these barcodes'])
+        writer.writerow(['Receive Time', 'No specimens with these barcodes'])
         for row in data:
-            error_message = row[0]
+            receive_time = row[0]
+            error_message = row[1]
             if "No specimens with these barcodes:" in error_message:
                 barcodes_part = error_message.split("No specimens with these barcodes:")[1].strip()
                 barcodes = barcodes_part.split(', ')
                 for barcode in barcodes:
                     if barcode:
-                        writer.writerow([barcode])
+                        writer.writerow([receive_time, barcode])
 
 def fetch_data(db_config):
     try:
@@ -27,13 +70,21 @@ def fetch_data(db_config):
             port=db_config['PORT']
         )
         cursor = connection.cursor()
+
+        # Calculate the previous day's start and end time
+        previous_day = datetime.now() - timedelta(days=1)
+        start_datetime = previous_day.replace(hour=0, minute=0, second=0, microsecond=0)
+        end_datetime = previous_day.replace(hour=23, minute=59, second=0, microsecond=0)
+
         query = """
-        SELECT error
+        SELECT receive_time, error
         FROM os_strata_freezer_events
-        WHERE process_time > %s AND process_time < %s AND error IS NOT NULL;
+        WHERE receive_time > %s
+        AND receive_time < %s
+        AND error LIKE '%No specimens with these barcodes%'
+        AND error IS NOT NULL;
         """
-        start_datetime = f"{db_config['START_DATE']} 00:00:00"
-        end_datetime = f"{db_config['END_DATE']} 23:59:00"
+
         cursor.execute(query, (start_datetime, end_datetime))
         results = cursor.fetchall()
         cursor.close()
@@ -53,7 +104,7 @@ def read_config(config_file):
             if '=' in line:
                 key, value = line.split('=', 1)
                 db_config[key.strip()] = value.strip().strip("'")
-    required_keys = ['DB_USER', 'DB_PASSWORD', 'HOST', 'DB_NAME', 'START_DATE', 'END_DATE']
+    required_keys = ['DB_USER', 'DB_PASSWORD', 'HOST', 'DB_NAME', 'SENDER_EMAIL', 'RECEIVER_EMAIL', 'EMAIL_PASSWORD']
     for key in required_keys:
         if key not in db_config:
             raise ValueError(f"Missing required configuration key: {key}")
@@ -67,16 +118,40 @@ def main():
     parser = argparse.ArgumentParser(description='Process database configuration and query data.')
     parser.add_argument('config_file', type=str, help='Path to the configuration file')
     args = parser.parse_args()
+
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     output_file = f"unsuccessful_barcodes_{timestamp}.csv"
+
     try:
         db_config = read_config(args.config_file)
         data = fetch_data(db_config)
         if not data:
-            print("No data found or there was an error fetching data.")
+            print("No specimens failed  yesterday...")
         else:
             write_to_csv(data, output_file)
             print(f"Data has been written to {output_file}")
+            
+            previous_day = datetime.now() - timedelta(days=1)
+            previous_day_str = previous_day.strftime("%Y-%m-%d")
+
+            subject = "Unsuccessful Barcodes Report"
+            body = f"""
+                    <html>
+                        <body>
+                        <p>Hello,</p>
+                        <p>
+                            Please find below attached report of specimen barcodes, those are not present in OpenSpecimen.<br>
+                            The report was generated on {previous_day_str}.
+                        </p>
+                        <p>Thanks.</p>
+                        </body>
+                    </html>
+                    """
+            senderEmail = db_config['SENDER_EMAIL']
+            receiverEmail = db_config['RECEIVER_EMAIL']
+            emailPassword = db_config['EMAIL_PASSWORD']
+
+            sendEmail(subject, body, senderEmail, receiverEmail, emailPassword, output_file)
     except ValueError as ve:
         print(f"Configuration error: {ve}")
     except Exception as e:
@@ -84,3 +159,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
